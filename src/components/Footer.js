b4107afb161d9dde401505bf0ca670766c7cb15b
mika_laptop_windows <charmikadev@gmail.com>
2022-08-15 18:01:43 +0100

    LogIn component html+css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,30 @@
 import React from "react";
-import '../styles/Footer..scss'
+import '../styles/Footer.scss'
 
 const Footer = () => {
     return (
         <footer>
             <div className='footer-container'>
-                <div> English (UK) </div>
-                <div>
-                    <ul className='pageFooterLinkList'>
-                        <li>Sign Up</li>
-                        <li>Log In</li>
-                        <li>Mika's GitHub</li>
-                    </ul>
-                </div>
-                <div>
-                    <p>Mika © 2022</p>
-                </div>
+                <ul className='language-list'>
+                    <li> English (UK) </li>
+                    <li>Polski</li>
+                    <li>Español</li>
+                    <li>Français</li>
+                    <li>Italiano</li>
+                    <li>Lietuvių</li>
+                    <li>Română</li>
+                    <li>中文(简体)</li>
+                    <li>Português</li>
+                    <li>Deutsch</li>
+                </ul>
+                <ul className='pageFooterLinkList'>
+                    <a href='/sign-up'><li>Sign Up</li></a>
+                    <a href='/'><li>Log In</li></a>
+                    <a href='https://github.com/Mikadev1996'><li>Mika's GitHub</li></a>
+                </ul>
+                <ul>
+                    <li>Mika © 2022</li>
+                </ul>
             </div>
         </footer>
     )
--- src/components/pages/LogIn.js
+++ src/components/pages/LogIn.js
@@ -1,18 +1,47 @@
 import '../../styles/LogIn.scss';
-import React from "react";
+import React, {useState} from "react";
 import Footer from "../Footer";
 
 const LogIn = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div className='app'>
-            <div className='title-container'>
-                <h1>Facebook clone</h1>
-                <p>Facebook helps you connect and share with the people in your life.</p>
-            </div>
-            <div className='form-container'>
-                <form>
+            <div className="login-content">
+                <div className='title-container'>
+                    <h1>Mika's facebook</h1>
+                    <p>Facebook helps you connect and share with the people who saw this Git Repo.</p>
+                </div>
+                <div id="form-container">
+                    <form className='account-form'>
+                        <div className="form-control">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" placeholder="Username" id="username" name='username' onChange={e => setUsername(e.target.value)}/>
+                            <small>Error Message</small>
+                        </div>
 
-                </form>
+                        <div className="form-control">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" placeholder="Password" id="password" maxLength="16" name='password' onChange={e => setPassword(e.target.value)}/>
+                            <small>Error Message</small>
+                        </div>
+
+                        <div>
+                            <button className='form-submit' type="submit">Log In</button>
+                        </div>
+                        <div>
+                            <button id='facebook-login' className='form-submit' type="submit">Log In with real Facebook</button>
+                        </div>
+                        <div>
+                            <button id='test-account-login' className='form-submit' type="submit">Test Account</button>
+                        </div>
+                    </form>
+                    <div className='hr-container'><hr className='form-hr'/></div>
+                    <div className='create-new-container'>
+                        <button className='form-submit'>Create New Account</button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
