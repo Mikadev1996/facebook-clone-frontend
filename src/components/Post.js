ac908f9ff2934cb6f65acd5791963a480333487e
mika_laptop_windows <charmikadev@gmail.com>
2022-08-17 11:25:58 +0100

    Post css

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,27 +5,31 @@ const Post = () => {
 
     return (
         <div className='post-container'>
-            <div className='post-header'>
-                <span className='icon-button'>🐶</span>
-                <div>
-                    <p>Post Title</p>
-                    <p>timestamp Ago · Location</p>
+            <div className='post-1'>
+                <div className='post-header'>
+                    <span className='icon-button'>🐶</span>
+                    <div>
+                        <p>Post Title</p>
+                        <p>timestamp Ago · Location</p>
+                    </div>
                 </div>
-            </div>
-            <div className='post-content'>
-
-            </div>
-            <div className="post-footer">
-                <div className='post-like-container'>
-                    <p>Like</p>
+                <div className='post-content'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Aliquam aliquid amet aspernatur beatae, cum dignissimos facere minima minus,
+                        nesciunt porro ullam voluptate.
+                        Cupiditate debitis eum necessitatibus quibusdam rerum sequi tempora.</p>
                 </div>
-                <div className='post-info'><p>Likes logo 17</p><p>X comments</p></div>
-                <div className="post-comment-container">
-                    <p>Comment</p>
-                </div>
-                <div className='post-add-comment-container'>
-                    <span className='icon-button-small'>🤑</span>
-                    <p>Write a comment...</p>
+                <div className="post-footer">
+                    <div className='post-info'><p>Likes logo 17</p><p>X comments</p></div>
+                    <div className='post-like-container'>
+                        <p>Like</p>
+                        <p>Comment</p>
+                        <p>Share</p>
+                    </div>
+                    <div className='post-add-comment-container'>
+                        <span className='icon-button-small'>🤑</span>
+                        <p>Write a comment...</p>
+                    </div>
                 </div>
             </div>
         </div>
